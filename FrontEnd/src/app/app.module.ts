d2779574469f3899f55ef0638378c62e1afac4c1
bautiarmanicode <bautiarmanienergia@gmail.com>
2022-07-06 16:35:13 -0300

    Creacion de modulos

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { Educacion2Component } from './components/educacion2/educacion2.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    BannerComponent,
+    Educacion2Component,
+    ExperienciaComponent,
+    FooterComponent,
+    HardysoftComponent,
+    LogoApComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
