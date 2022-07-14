07d819ac509f37730a8c37c9be73e85a92ad81c1
bautiarmanicode <bautiarmanienergia@gmail.com>
2022-07-13 21:39:30 -0300

    Carga Backend

---
+++ BackEnd/src/main/java/com/portfolioBau/BBB/ArmaniApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioBau.BBB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmaniApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolioBau/BBB/ArmaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioBau.BBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
